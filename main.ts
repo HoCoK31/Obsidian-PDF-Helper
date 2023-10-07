f3a2c7aeb15bb1761a2548a8522c321895856a65
HoCoK31 <MuramasaLight@gmail.com>
2023-10-07 18:51:10 +0300

    feat: :sparkles: Add thumbnails and page count of PDF

--- main.ts
+++ main.ts
@@ -1,134 +1,104 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from "obsidian";
+import { getAPI } from "obsidian-dataview";
 
-// Remember to rename these classes and interfaces!
+const dataviewAPI = getAPI();
+const mainPattern = new RegExp(/:pdf-[^:]*:[^:]*:(?:[0-9]*:)?$/);
 
-interface MyPluginSettings {
-	mySetting: string;
+export default class PdfHelper extends Plugin {
+	async onload() {
+		this.registerMarkdownPostProcessor(async (element, context) => {
+			const pElements = element.querySelectorAll("p");
+
+			for (let index = 0; index < pElements.length; index++) {
+				const pElement = pElements.item(index);
+				const text = pElement.innerText.trim();
+
+				const match = mainPattern.exec(text);
+
+				if (match?.length != 1)
+					continue;
+
+				const params = match[0].split(":");
+
+				let url = params[2];
+				url = context.frontmatter[url] ?? url;
+				if (url.startsWith("[["))
+					url = url?.substring(2, url.length - 2);
+				if (!url.match(/.pdf$/))
+					continue;
+				url = this.app.metadataCache.getFirstLinkpathDest(url, "")?.path || "";
+				if (!url)
+					continue;
+
+				const obsidian = require("obsidian");
+				const pdfjs = await (0, obsidian.loadPdfJs)();
+
+				const arrayBuffer = await this.app.vault.adapter.readBinary(url);
+				const buffer = new Uint8Array(arrayBuffer);
+				const pdf = await pdfjs.getDocument(buffer).promise;
+
+				switch (params[1]) {
+					case "pdf-thumbnail":
+						let pageNumber: number = parseInt(params[3]) || 1;
+						if (pageNumber > pdf.numPages)
+							pageNumber = 1;
+
+						const page = await pdf.getPage(pageNumber);
+
+						context.addChild(new pdfThumbnail(pElement, page));
+						break;
+					case "pdf-page-count":
+						context.addChild(new pdfPageCount(pElement, pdf.numPages));
+						break;
+					default:
+						break;
+				}
+
+			}
+		});
+	}
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import { MarkdownRenderChild } from "obsidian";
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export class pdfThumbnail extends MarkdownRenderChild {
+	page: any;
+
+	constructor(containerEl: HTMLElement, page: any) {
+		super(containerEl);
+		this.page = page;
+	}
 
 	async onload() {
-		await this.loadSettings();
+		const canvas = document.createElement("canvas");
+		canvas.style.width = "100%";
+		canvas.style.margin = "0em";
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		const context = canvas.getContext("2d");
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		const baseViewportWidth = this.page.getViewport({ scale: 1 }).width;
+		const baseScale = canvas.clientWidth ? canvas.clientWidth / baseViewportWidth : 1;
+		const viewport = this.page.getViewport({ scale: baseScale });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		canvas.height = viewport.height;
+		canvas.width = viewport.width;
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		const renderContext = { canvasContext: context, viewport };
+		this.page.render(renderContext);
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+		this.containerEl.replaceWith(canvas);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+export class pdfPageCount extends MarkdownRenderChild {
+	pageNum: number;
+
+	constructor(containerEl: HTMLElement, pageNum: number) {
+		super(containerEl);
+		this.pageNum = pageNum;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	async onload() {
+		this.containerEl.innerText = this.containerEl.innerText.replace(mainPattern, this.pageNum.toString());
 	}
 }
